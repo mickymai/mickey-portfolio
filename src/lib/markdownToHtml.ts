5dfea43663ed7840309cc3ef11db9571e7d4f79b
jeremyn-nmq <quannguyen.jn@gmail.com>
2025-04-04 01:02:34 +0700

    Remove static

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,10 @@
 import { remark } from 'remark'
 import html from 'remark-html'
 
-export default async function markdownToHtml(markdown: string) {
+export default async function markdownToHtml(markdown: string = '') {
+  if (markdown.length === 0) {
+    return ''
+  }
   const result = await remark().use(html).process(markdown)
   return result.toString()
 }
